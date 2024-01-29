e36c07625e3562f745c0ef40b78eff00548e5c6d
Triksteringog <150711092+Triksteringog@users.noreply.github.com>
2024-01-29 20:23:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".allShapes", {
+//     x: 50,
+//     duration: 3,
+//     ease: "none",
+//     repeat: -1,
+//     yoyo: true,
+//     scrollTrigger: {
+//         trigger: ".allShapes",
+//         start: "top center",
+//         end: "bottom center",
+//     },
+// });
